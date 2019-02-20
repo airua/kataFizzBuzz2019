216b35a8419fae46ef7498dc3213d55c58dfb8d6
airua <auriavaci@gmail.com>
2019-02-20 09:54:22 +0100

    primer commit

---
+++ test/com/cifpcarballeira/katas/fizzbuzz/tests/Testfizzbuzz.java
@@ -0,0 +1,26 @@
+package com.cifpcarballeira.katas.fizzbuzz.tests;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dam227
+ */
+public class Testfizzbuzz {
+    
+    public Testfizzbuzz() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
